5acf9cad8236591a6fabbe6d170fedf4108fca1c
jda20-Earth <jda20@stmarys-ca.edu>
2025-09-26 22:58:14 -0700

    Work

--- lab2.sql
+++ lab2.sql
@@ -1,23 +1,102 @@
 --1.	Display the invoice number, the invoice date, the customer id, and the customer name for each order in the database.
-SELECT invoice_num, invoice_date, cust_id,  FROM invoice;
---2.	Display the invoice number, the customer id, and the customer name for each order placed on September 12th, 2007.
+SELECT invoice_num, invoice_date, customer.cust_id, cust_name
+FROM invoice, customer
+WHERE customer.cust_id = invoice.cust_id;
 
+-- Or, 
+-- this is EQUIJOIN
+
+SELECT invoice_num, invoice_date, c.cust_id, cust_name 
+FROM invoice AS i, customer AS c
+WHERE c.cust_id = i.cust_id;
+
+--or
+--with inner join
+
+SELECT invoice_num, invoice_date, customer.cust_id, cust_name
+FROM invoice INNER JOIN customer ON invoice.cust_id = customer.cust_id;
+
+--or
+--with outer join with the right, note the precedence afforded by the keyword RIGHT.
+-- Will show partners the right has has with the left, and those from the right without a partner. 
+SELECT invoice_num, invoice_date, customer.cust_id, cust_name
+FROM invoice RIGHT OUTER JOIN customer ON invoice.cust_id = customer.cust_id;
+
+
+--2.	Display the invoice number, the customer id, and the customer name for each order placed on September 12th, 2007.
+SELECT invoice_num, invoice_date, c.cust_id, cust_name
+FROM invoice as i, customer as c
+WHERE c.cust_id = i.cust_id
+AND invoice_date = '12-SEP-07';
 --3.	Display the invoice number, the invoice date, the product id, the number of units ordered, and the line price for each line in each order.
+-- SELECT * FROM line;
+-- SELECT * FROM invoice;
+SELECT invoice.invoice_num, invoice_date, line.prod_id, prod_quantity, line_price
+FROM invoice, line, product
+WHERE invoice.invoice_num = line.invoice_num
+AND product.prod_id = line.prod_id
+ORDER BY invoice.invoice_num, product.prod_id, line_price;
+
 
 --4.	Display the id and the name of each customer that placed an order on September 12th, 2007, using the IN operator in your query.
-
+SELECT cust_id, cust_name 
+FROM customer
+WHERE cust_id IN 
+(	SELECT cust_id 
+	FROM invoice 
+	WHERE invoice_date = '12-SEP-07');
 --5.	Display the id and the name of each customer that placed an order on September 12th, 2007, using the EXISTS operator in your query.
-
+-- SELECT * FROM customer;
+SELECT cust_id, cust_name
+FROM customer
+WHERE EXISTS (
+	SELECT 1
+	FROM invoice
+	WHERE invoice_date = '12-SEP-07'
+	AND invoice.cust_id = customer.cust_id
+);
 --6.	Display the id and the name of each customer that did not place an order on September 12th, 2007.   (Be careful in performing this query.)
- 
-
+-- SELECT * FROM customer;
+ SELECT customer.cust_id, cust_name
+ FROM customer, invoice
+ WHERE customer.cust_id = invoice.cust_id
+ AND invoice_date != '12-SEP-07'
+ GROUP BY customer.cust_id
+ ORDER BY customer.cust_id;
 --7.	Display the invoice number, the invoice date, the product id, the product description, and the product type for each line in each order.
+-- SELECT * FROM invoice
+-- SELECT * FROM product
+-- SELECT * FROM line
+
+SELECT invoice.invoice_num, invoice_date, product.prod_id, prod_desc, prod_type
+FROM invoice, product, line
+WHERE invoice.invoice_num = line.invoice_num
+AND product.prod_id = line.prod_id;
 
 --8.	Display the same data as in question 7, but order the display by product type.  Within each type, order the display by invoice number.
+SELECT invoice.invoice_num, invoice_date, product.prod_id, prod_desc, prod_type
+FROM invoice, product, line
+WHERE invoice.invoice_num = line.invoice_num
+AND product.prod_id = line.prod_id
+ORDER BY prod_type, invoice.invoice_num;
+
 
 --9.	Display the sales representative's id, last name, and first name of each representative who represents, at a minimum, one customer whose credit is $10,000 using a subquery.
- 
+-- SELECT * FROM customer;
+SELECT rep_id, rep_lname, rep_fname
+FROM rep
+WHERE rep_id IN (
+	SELECT rep_id
+	FROM customer
+	WHERE cust_limit = 10000
+)
+ORDER BY rep.rep_id;
 --10.	Display the same data as in the previous question without using a subquery.
+SELECT rep.rep_id, rep_lname, rep_fname
+FROM rep, customer
+WHERE rep.rep_id = customer.rep_id
+AND cust_limit = 10000
+GROUP BY rep.rep_id;
 
 --11.	Display the id and the name of each customer with a current order for a Blender.
 
